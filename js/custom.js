286476aafe3d019d01292bd3893ed4e4ab4a3a24
daniel <deepakswami07@gmail.com>
2015-03-30 16:46:02 -0400

    add colloapse bar on selection to hide

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,11 @@
 $('.carousel').carousel({
     interval: false
 }) 
+
+/////////////////////////////////////////
+// begin navbar collapse on selection ///
+$('.navbar-collapse').click('li', function() {
+    $('.navbar-collapse').collapse('hide');
+});
+// end navbar collapse on selection ///
+///////////////////////////////////////
